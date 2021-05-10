56032b972258959e77b76aa438ad0875d662a611
“salamzakeer” <salamzakeer123@gmail.com>
2021-05-10 17:02:21 +0530

    1ST COMMIT

---
+++ PAF_CLIENT_GB/src/com/productService.java
@@ -0,0 +1,82 @@
+package com;
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+
+import model.product;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+@Path("/product")
+public class productService {
+
+	product Obj = new product();
+	@GET
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_HTML)
+	public String readItems()
+	 {
+
+	 return Obj.readItems();
+	 }
+
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertItem(@FormParam("projectID") Integer projID,
+	 @FormParam("projName") String projName,
+	 @FormParam("description") String description,
+	 @FormParam("area") String area,
+	 @FormParam("resName") String resName,
+	 @FormParam("resID") String resID,
+	 @FormParam("price") float price)
+	{
+
+	 String output = Obj.insertproductdetails(projID, projName, description, area, resName, resID, price);
+	return output;
+	}
+
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateProduct(String proj)
+	{
+	//Convert the input string to a JSON object
+	 JsonObject Object = new JsonParser().parse(proj).getAsJsonObject();
+	//Read the values from the JSON object
+	 Integer projID = Object.get("projID").getAsInt();
+	 String projName = Object.get("projName").getAsString();
+	 String description = Object.get("description").getAsString();
+	 String area = Object.get("area").getAsString();
+	 String resName = Object.get("resName").getAsString();
+	 String resID = Object.get("resID").getAsString();
+	 float price = Object.get("price").getAsFloat();
+
+	 String output = Obj.updateproductdetails(projID, projName, description, area, resName, resID, price);
+	return output;
+	}
+
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteItem(String proj)
+	{
+	//Convert the input string to an XML document
+	 Document doc = Jsoup.parse(proj, "", Parser.xmlParser());
+
+	 String projID = doc.select("projID").text();
+	 String output = Obj.deleteItem(projID);
+	return output;
+	}
+
+
+
+}
---
+++ PAF_CLIENT_GB/src/model/product.java
@@ -0,0 +1,170 @@
+package model;
+import java.sql.*;
+public class product {
+
+	//A common method to connect to the DB
+	private Connection connect()
+	 {
+	 Connection con = null;
+	 try
+	 {
+	 Class.forName("com.mysql.jdbc.Driver");
+
+
+	 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gadgetbadget", "root", "");
+	 }
+	 catch (Exception e)
+	 {e.printStackTrace();}
+	 return con;
+	 }
+
+	public String insertproductdetails(Integer projID, String projName, String description, String area, String resName, String resID, float price)
+	 {
+	 String output = "";
+	 try
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for inserting."; }
+	 // create a prepared statement
+	 String query = "INSERT INTO product (projID, projName, description, area, resID, resName, price) VALUES (?, ?, ?, ?, ?, ?, ?)";
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+
+	 preparedStmt.setInt(1, projID);
+	 preparedStmt.setString(2, projName);
+	 preparedStmt.setString(3, description);
+	 preparedStmt.setString(4, area);
+	 preparedStmt.setString(5, resName);
+	 preparedStmt.setString(6, resID);
+	 preparedStmt.setFloat(7, price);
+	// execute the statement
+
+	 preparedStmt.execute();
+	 con.close();
+	 output = "Inserted successfully";
+	 }
+	 catch (Exception e)
+	 {
+	 output = "Error while inserting the product.";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 }
+	public String readItems()
+	 {
+	 String output = "";
+	 try
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for reading."; }
+	 // Prepare the html table to be displayed
+
+	 output = "<table border='1' align='center'><tr><th>Product ID</th>" +
+	 "<th>Product Name</th>" +
+	 "<th>Product Description</th>"+
+	 "<th>Product Area</th>"+
+	 "<th>Researcher Name</th>"+
+	 "<th>Researcher ID</th>"+
+	 "<th>Price</th>"+
+	 "<th>Update</th><th>Remove</th></tr>";
+
+	 String query = "select * from product";
+	 Statement stmt = con.createStatement();
+	 ResultSet rs = stmt.executeQuery(query);
+
+	 while (rs.next())
+	 {
+		//projID projName description area resID resName price
+	 String projID = Integer.toString(rs.getInt("projID"));
+	 String projName = rs.getString("projName");
+	 String description = rs.getString("description");
+	 String area =rs.getString("area");
+	 String resID = rs.getString("resID");
+	 String resName = rs.getString("resName");
+	 String price = String.valueOf(rs.getFloat("price"));
+
+	 output += "<tr><td>" + projID + "</td>";
+	 output += "<td>" + projName + "</td>";
+	 output += "<td>" + description + "</td>";
+	 output += "<td>" + area + "</td>";
+	 output += "<td>" + resID + "</td>";
+	 output += "<td>" + resName + "</td>";
+	 output += "<td>" + price + "</td>";
+	 // buttons
+	 output += "<td><input name='btnUpdate' type='button' value='Update' class='btn btn-secondary'></td>"
+			 + "<td><form method='post' action='customer.jsp'>"
+			 + "<input name='btnRemove' type='submit' value='Remove' class='btn btn-danger'>"
+			 + "<input name='customerID' type='hidden' value='" + projID
+			 + "'>" + "</form></td></tr>";
+			 }
+			 con.close();
+
+			 output += "</table>";
+			 } 
+	 catch (Exception e)
+	 {
+	 output = "Error while reading the products.";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 }
+	public String updateproductdetails(Integer projID, String projName, String description, String area, String resName, String resID, float price)
+
+	 {
+	 String output = "";
+	 try
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for updating."; }
+
+	 String query = "UPDATE product SET  projName=?, description=?, area=?, resID=?, resName=?, price=? WHERE projID=?";
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+	 // binding values
+
+	 preparedStmt.setString(1, projName);
+	 preparedStmt.setString(2, description);
+	 preparedStmt.setString(3, area);
+	 preparedStmt.setString(4, resName);
+	 preparedStmt.setString(5, resID);
+	 preparedStmt.setFloat(6, price);
+	 preparedStmt.setInt(7, projID);
+	 // execute the statement
+	 preparedStmt.execute();
+	 con.close();
+	 output = "Updated successfully";
+	 }
+	 catch (Exception e)
+	 {
+	 output = "Error while updating the item.";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 }
+	public String deleteItem(String customerID)
+	 {
+	 String output = "";
+	 try
+	 {
+	 Connection con = connect();
+	 if (con == null)
+	 {return "Error while connecting to the database for deleting."; }
+	 // create a prepared statement
+	 String query = "DELETE FROM product  WHERE projID=?";
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+	 // binding values
+	 preparedStmt.setInt(1, Integer.parseInt(customerID));
+	 // execute the statement
+	 preparedStmt.execute();
+	 con.close();
+	 output = "Deleted successfully";
+	 }
+	 catch (Exception e)
+	 {
+	 output = "Error while deleting the item.";
+	 System.err.println(e.getMessage());
+	 }
+	 return output;
+	 }
+	}  
